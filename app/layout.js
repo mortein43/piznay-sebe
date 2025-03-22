d19de0268fdc3c1d47ba5fffa2b337e7fe11859e
mortein43 <mortein43@gmail.com>
2025-03-22 13:31:57 +0200

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/styles/globals.scss";
+import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
   title: "Пізнай себе",
   description: "Щоб пізнати себе краще",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
