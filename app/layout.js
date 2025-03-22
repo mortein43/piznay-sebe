978077978dda8f4fdf871c4fa7c7bb495ccfd577
mortein43 <mortein43@gmail.com>
2025-03-22 13:54:58 +0200

    add google search engine

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="6A0SGqXlYwu_gQDr96O790GIZ6-j3DhJR3f5mUFhi7c"
+        />
+      </head>
       <body className={`antialiased`}>
         {children}
         <Analytics />
